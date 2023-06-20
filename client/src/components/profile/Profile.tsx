0cabe169dc4b7d07d17e74d568c9075d63f4213e
mrcxmrj <marcin.zurawel@gmail.com>
2023-06-20 19:12:20 +0200

    move rental fetching logic to a custom hook

--- client/src/components/profile/Profile.tsx
+++ client/src/components/profile/Profile.tsx
@@ -4,43 +4,13 @@ import { Button } from "@chakra-ui/react";
 import { Navigate } from "react-router-dom";
 import RentalList from "../RentalList";
 import { Rental } from "../../types/interfaces";
+import { useFetchRentals } from "../../hooks/useFetchRentals";
 
 export const Profile: React.FC = () => {
     const { user, setUser } = useContext(AuthContext);
-    const [rentals, setRentals] = useState<Rental[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState("");
-    const apiKey: string = process.env.REACT_APP_API_URL || "";
-
-    useEffect(() => {
-        const fetchRentals = async () => {
-            console.log("fetching...");
-            try {
-                const response = await fetch(
-                    `${apiKey}/user/rentals/${user!.id}`,
-                    {
-                        method: "GET",
-                        headers: {
-                            "Content-Type": "application/json",
-                            "x-access-token": user?.accessToken as string,
-                        },
-                    }
-                );
-                if (response.ok) {
-                    const data = await response.json();
-                    setRentals(data);
-                    setLoading(false);
-                } else {
-                    const errorData = await response.json();
-                    setError(errorData.message);
-                }
-            } catch (error) {
-                console.error(error);
-                setLoading(false);
-            }
-        };
-        fetchRentals();
-    }, [user, apiKey]);
+    const { rentals, loading, error } = useFetchRentals(
+        `/user/rentals/${user?.id}`
+    );
 
     return user ? (
         <>
--- client/src/hooks/useFetchRentals.ts
+++ client/src/hooks/useFetchRentals.ts
@@ -1,32 +1,19 @@
 import { useContext, useEffect, useState } from "react";
-import {
-    Box,
-    Button,
-    Heading,
-    List,
-    ListItem,
-    Spinner,
-    Text,
-} from "@chakra-ui/react";
-import { Navigate } from "react-router-dom";
 import { Rental } from "../types/interfaces";
-import RentalList from "./RentalList";
 import { AuthContext } from "../context/AuthContext";
-import { useFetchRentals } from "../hooks/useFetchRentals";
 
-export const AdminPanel: React.FC = () => {
-    const { rentals, loading, error } = useFetchRentals("/rentals");
-    /* const [rentals, setRentals] = useState<Rental[]>([]);
+export const useFetchRentals = (relativeRoute: string) => {
+    const { user, setUser } = useContext(AuthContext);
+    const [rentals, setRentals] = useState<Rental[]>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState("");
     const apiKey: string = process.env.REACT_APP_API_URL || "";
-    const { user } = useContext(AuthContext);
 
     useEffect(() => {
         const fetchRentals = async () => {
             console.log("fetching...");
             try {
-                const response = await fetch(`${apiKey}/rentals`, {
+                const response = await fetch(apiKey + relativeRoute, {
                     method: "GET",
                     headers: {
                         "Content-Type": "application/json",
@@ -40,7 +27,6 @@ export const AdminPanel: React.FC = () => {
                 } else {
                     const errorData = await response.json();
                     setError(errorData.message);
-                    setLoading(false);
                 }
             } catch (error) {
                 console.error(error);
@@ -48,30 +34,7 @@ export const AdminPanel: React.FC = () => {
             }
         };
         fetchRentals();
-    }, [user, apiKey]);
- */
-    if (loading) {
-        return (
-            <Box textAlign="center" mt="4">
-                <Spinner size="xl" />
-            </Box>
-        );
-    }
+    }, [user, apiKey, relativeRoute]);
 
-    if (error) {
-        return (
-            <Box textAlign="center" mt="4">
-                <Text color="red.500">{error}</Text>
-            </Box>
-        );
-    }
-
-    return (
-        <Box>
-            <Heading as="h1" mb="4">
-                All Rentals
-            </Heading>
-            <RentalList rentals={rentals} loading={loading} error={error} />
-        </Box>
-    );
+    return { rentals, loading, error };
 };
