f5d504c930648e330189d362e17202eb3d160c00
mrcxmrj <marcin.zurawel@gmail.com>
2023-06-20 19:13:23 +0200

    remove unnecessary stuff from admin panel

--- client/src/components/AdminPanel.tsx
+++ client/src/components/AdminPanel.tsx
@@ -16,55 +16,6 @@ import { useFetchRentals } from "../hooks/useFetchRentals";
 
 export const AdminPanel: React.FC = () => {
     const { rentals, loading, error } = useFetchRentals("/rentals");
-    /* const [rentals, setRentals] = useState<Rental[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState("");
-    const apiKey: string = process.env.REACT_APP_API_URL || "";
-    const { user } = useContext(AuthContext);
-
-    useEffect(() => {
-        const fetchRentals = async () => {
-            console.log("fetching...");
-            try {
-                const response = await fetch(`${apiKey}/rentals`, {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "x-access-token": user?.accessToken as string,
-                    },
-                });
-                if (response.ok) {
-                    const data = await response.json();
-                    setRentals(data);
-                    setLoading(false);
-                } else {
-                    const errorData = await response.json();
-                    setError(errorData.message);
-                    setLoading(false);
-                }
-            } catch (error) {
-                console.error(error);
-                setLoading(false);
-            }
-        };
-        fetchRentals();
-    }, [user, apiKey]);
- */
-    if (loading) {
-        return (
-            <Box textAlign="center" mt="4">
-                <Spinner size="xl" />
-            </Box>
-        );
-    }
-
-    if (error) {
-        return (
-            <Box textAlign="center" mt="4">
-                <Text color="red.500">{error}</Text>
-            </Box>
-        );
-    }
 
     return (
         <Box>
